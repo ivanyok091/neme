059f03e1f5c5882161daa8504fd55b66cbba843f
ivanyok091 <113350110+ivanyok091@users.noreply.github.com>
2025-11-02 14:43:10 +0500

    Update telegram.bot.py

--- telegram.bot.py
+++ telegram.bot.py
@@ -1,32 +1,10 @@
 import telebot
 import requests
 
-bot = telebot.TeleBot('5501488079:AAEPH7EL2SdrKxg7BgF4s0Dkrv8F8Ut-2QM')
-
-def ipfunc(ip):
-    try:
-        req = requests.get(f"http://ip-api.com/json/{ip}").json()
-        return f"""
-        Подключение: {req['status']}
-        Страна: {req['country']}
-        Город: {req['city']}
-        Провайдер: {req['isp']}
-        Часовой пояс: {req['timezone']}
-        """
-    except KeyError:
-        return 'Это не IP адрес'
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'привет {0.first_name} я бот который узнает где ты находишься по айпи'
-                          'бот был создан для агро школьников из майнкравта'.format(message.from_user))
-
-@bot.message_handler(commands=['ip'])
-def ipcommand(message):
-    bot.reply_to(message,'кажите ваш айпэ')
     bot.register_next_step_handler(message, ipinfo)
 
 def ipinfo(message):
     bot.reply_to(message, ipfunc(message.text))
+
 
 bot.polling(none_stop=True, interval=0)
