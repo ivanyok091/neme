1702ba88d78140f60b5133217ad60ffc6f10c2e4
ivanyok091 <113350110+ivanyok091@users.noreply.github.com>
2025-11-02 14:43:57 +0500

    Update telegram.bot.py

--- telegram.bot.py
+++ telegram.bot.py
@@ -1,10 +1,1 @@
-import telebot
-import requests
 
-    bot.register_next_step_handler(message, ipinfo)
-
-def ipinfo(message):
-    bot.reply_to(message, ipfunc(message.text))
-
-
-bot.polling(none_stop=True, interval=0)
